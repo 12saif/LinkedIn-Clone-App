86dc9c76c85bfc981da4ee52e2f59171ef7d3067
md saif <mohdsaif9170613062@gmail.com>
2023-03-06 01:39:15 +0530

    Create Headre Components and Completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="App">
-
       {/* Header */}
       <Header />
       {/* App Body */}
-      {/* Sidebar */}
-      {/* Feed */}
-      {/* Widgets */}
+      <div className="app__body">
+        {/* Sidebar */}
+        <Sidebar />
+        {/* Feed */}
+        {/* Widgets */}
+      </div>
     </div>
   );
 }
